0ef2133c84aef4deaab85fb63de875577dc6f5cf
pkavrakov <petikavrakov@gmail.com>
2025-05-25 21:49:25 +0300

    Scroll to top btn in JS file, not in HTML

---
+++ Petar/scrolltotop.js
@@ -0,0 +1,12 @@
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {
+  if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
